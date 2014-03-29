10f57e854f97fc16f8ba5012723a6e02ee87c118
David Meads <david.meads@cengage.com>
2014-03-29 16:29:31 -0400

    Starting to add Grunt and cleaned up some lint errors.

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            // define the files to lint
+            files: ['gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+            // configure JSHint (documented at http://www.jshint.com/docs/)
+            options: {
+                // more options here if you want to override JSHint defaults
+                globals: {
+                    console: true,
+                    require: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', ['jshint']);
+};
