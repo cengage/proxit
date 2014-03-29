022f6f73863347c2e85ebdb19db113338d0592ea
David Meads <david.meads@cengage.com>
2014-03-29 16:49:17 -0400

    Starting to add testing to proxit.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,25 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            // define the files to lint
             files: ['gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
-            // configure JSHint (documented at http://www.jshint.com/docs/)
             options: {
-                // more options here if you want to override JSHint defaults
                 globals: {
                     console: true,
                     require: true
                 }
             }
+        },
+        mochaTest: {
+            options: {
+                reporter: 'spec'
+            },
+            src: ['test/**/*_spec.js']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'mochaTest']);
 };
