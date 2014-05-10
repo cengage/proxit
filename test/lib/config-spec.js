e60a41b9f7e850147239ceebe1c6ce1756e990ae
David Meads <dbmeads@hotmail.com>
2014-05-10 13:40:41 -0400

    Starting to add some tests to config before extending it.

---
+++ test/lib/config-spec.js
@@ -0,0 +1,59 @@
+var chai = require('chai'),
+    spies = require('chai-spies'),
+    expect = chai.expect,
+    mockery = require('../mockery'),
+    fs = require('fs'),
+    rc = require('rc'),
+    path = require('path'),
+    sut = '../../lib/config',
+    _ = require('lodash'),
+    defaultConfig = JSON.parse(fs.readFileSync('.proxitrc'));
+
+chai.use(spies);
+
+describe('config', function() {
+    var config,
+        exists,
+        result;
+
+    beforeEach(function() {
+        mockery.setup({
+            allow: [sut, 'lodash', 'path', 'minimist'],
+            mock: {
+                'fs': {
+                    existsSync: function() {
+                        return exists;
+                    }
+                },
+                'rc': chai.spy(rc)
+            }
+        });
+
+        exists = false;
+    });
+
+    it('should return local config when nothing is passed', function() {
+        givenProxitJson();
+        whenConfigCalledWithNoParams();
+        thenConfigShouldEqualProxitJson();
+    });
+
+    afterEach(function() {
+        mockery.teardown();
+    });
+
+    function givenProxitJson() {
+        mockery.registerMock(process.cwd() + path.sep + 'proxit.json', _.cloneDeep(defaultConfig));
+        exists = true;
+    }
+
+    function whenConfigCalledWithNoParams() {
+        config = require(sut);
+        result = _.omit(config());
+    }
+
+    function thenConfigShouldEqualProxitJson() {
+        expect(result).to.eql(defaultConfig);
+        mockery.deregisterMock('../../proxit.json');
+    }
+});
---
+++ test/mockery.js
@@ -0,0 +1,26 @@
+var mockery = require('mockery'),
+    _ = require('lodash');
+
+module.exports = _.extend(mockery, {
+    setup: function(options) {
+        options = options || {};
+
+        this.enable(options.enable);
+
+        if (options.mock) {
+            this.registerMocks(options.mock);
+        }
+
+        if (options.allow) {
+            this.registerAllowables(options.allow);
+        }
+    },
+    registerMocks: function(mocks) {
+        for (var key in mocks) {
+            this.registerMock(key, mocks[key]);
+        }
+    },
+    teardown: function() {
+        this.disable();
+    }
+});
